2cbd34c679f26316beae8db8822d50a04ff665fb
Stephen Clements <mrevilg@gmail.com>
2021-04-04 10:05:32 -0500

    basic data types

--- main.go
+++ main.go
@@ -19,4 +19,23 @@ func main() {
 	fmt.Println("Try it out in the terminal!")
 	fmt.Println("go doc fmt.Println") // or time.Now
 
+	// cannot be update whilst running
+	const constant = "This is a const being called" 
+	fmt.Println(constant)
+
+	// literal unnamed value
+	fmt.Println("PI = ", 3.14159)
+ 
+	// constant named value
+	const pi = 3.14159
+	
+	// variable named value
+	var radius = 6
+
+	// Basic Data Types - int, float, complex
+	/*
+	int8, uint8, int16, uint16, int32 , uint32, int64, uint64, int, uint, uintptr
+	float32, float64
+	complex64, complex128
+	*/
 }
