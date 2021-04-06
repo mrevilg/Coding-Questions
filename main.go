d2b8b85d1b00117ebd54812f758c6540c26acf1e
Stephen Clements <mrevilg@gmail.com>
2021-04-06 07:37:32 -0500

    Print vs Println

--- main.go
+++ main.go
@@ -70,7 +70,13 @@ func main() {
 	fmt.Println("Publisher:", publisher, "Pgs:", pageNumber, "Yr:", year, "Grade:", grade)
 	fmt.Println()
 
+	// Println VS Print
 
+	fmt.Println("Let's first see how", "the Println() method works.")
+	fmt.Println("Notice that each statement adds a newline for us.")
+	fmt.Println("There's also a default space", "between the string arguments.")
+	fmt.Print("Print", "is", "different") // no space
+	fmt.Print("See?") // adds to last possible place
 
 
 }
