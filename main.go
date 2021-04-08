49d2c169f27e564269a108646e385319f9f15443
Stephen Clements <mrevilg@gmail.com>
2021-04-08 18:32:59 -0500

    More verbs

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
-import t "time"
+import (
+	"fmt"
+	t "time"
+)
 
 // This is a Comment
 
-/* 
+/*
 This is a comment block
 */
 
@@ -20,32 +22,35 @@ func main() {
 	fmt.Println("go doc fmt.Println") // or time.Now
 
 	// cannot be update whilst running
-	const constant = "This is a const being called" 
+	const constant = "This is a const being called"
 	fmt.Println(constant)
 
 	// literal unnamed value
 	fmt.Println("PI = ", 3.14159)
- 
+
 	// constant named value
 	const pi = 3.14159
-	
-	// variable named value,
+
+	/* variable named value,
+
 	var radius = 6
 	radius := 6
+
+	*/
 
 	// Basic Data Types - int, float, complex.
 
 	/*
-	int8, uint8, int16, uint16, int32 , uint32, int64, uint64, int, uint, uintptr
-	float32, float64
-	complex64, complex128
+		int8, uint8, int16, uint16, int32 , uint32, int64, uint64, int, uint, uintptr
+		float32, float64
+		complex64, complex128
 	*/
 
 	var publisher, writer, artist, title string
 	var year uint
 	var pageNumber uint
 	var grade float32
-	  
+
 	title = "Mr.GoToSleep"
 	writer = "Tracey Hatchet"
 	artist = "Jewel Tampson"
@@ -53,11 +58,11 @@ func main() {
 	year = 1997
 	pageNumber = 14
 	grade = 6.5
-  
+
 	fmt.Println(title, "written by", writer, "drawn by", artist)
 	fmt.Println("Publisher:", publisher, "Pgs:", pageNumber, "Yr:", year, "Grade:", grade)
 	fmt.Println()
-  
+
 	title = "Epic Vol. 1"
 	writer = "Ryan N. Shawn"
 	artist = "Phoebe Paperclips"
@@ -65,7 +70,7 @@ func main() {
 	year = 2013
 	pageNumber = 160
 	grade = 9.0
-  
+
 	fmt.Println(title, "written by", writer, "drawn by", artist)
 	fmt.Println("Publisher:", publisher, "Pgs:", pageNumber, "Yr:", year, "Grade:", grade)
 	fmt.Println()
@@ -76,13 +81,24 @@ func main() {
 	fmt.Println("Notice that each statement adds a newline for us.")
 	fmt.Println("There's also a default space", "between the string arguments.")
 	fmt.Print("Print", "is", "different") // no space
-	fmt.Print("See?") // adds to last possible place
-	
-	// %v - Interpolation
+	fmt.Print("See?")                     // adds to last possible place
+
+	// %v - Interpolation/Verbs
+
+	specialNum := 42
+	fmt.Printf("This value's type is %T.", specialNum) // Prints value type 'int'
+
+	quote := "To do or not to do"
+	fmt.Printf("This value's type is %T.", quote) // Prints value type 'string'
 
 	animal1 := "cat"
 	animal2 := "dog"
-	
 	fmt.Printf("Are you a %v or a %v person?", animal1, animal2) // v = 'verb'
+
+	Num1 := 18
+	Num2 := 3.8
+	fmt.Printf("You must be %d years old to vote.", Num1) // d = 'int to string'
+	fmt.Printf("You're averaging: %f.", Num2)             // f = 'int to string (float)'
+	fmt.Printf("You're averaging: %.2f.", Num2)           // f = 'int to string (precision float)'
 
 }
