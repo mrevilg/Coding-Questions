9125009e9234b89b31d2c774e0d00858a14651b6
Stephen Clements <mrevilg@gmail.com>
2021-04-27 16:57:05 -0500

    basic tuple

---
+++ tuple.py
@@ -0,0 +1,13 @@
+#Tuple - Immuateble list, positional elements
+myInfo = ('Stephen', '42', 'Techy')
+
+print(myInfo[0])
+print(myInfo[1])
+print(myInfo[-1])
+
+# Unpacking the Tuple
+name, age, Occupation = myInfo
+
+print(name)
+print(age)
+print(Occupation)
